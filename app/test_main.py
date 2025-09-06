f78d84cf8441a72e87183e2ca1f6cd7c5af66933
Yurii Palahitskyi <brebston7@gmail.com>
2025-09-06 19:33:29 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,41 @@
-# write your code here
+from app.main import check_password
+
+
+def test_valid_example() -> None:
+    assert check_password("Pass@word1") is True
+
+
+def test_too_short() -> None:
+    assert check_password("Str@ng") is False
+
+
+def test_only_letters_invalid() -> None:
+    assert check_password("qwertyui") is False
+
+
+def test_missing_uppercase() -> None:
+    assert check_password("password1@") is False
+
+
+def test_missing_digit() -> None:
+    assert check_password("Password@") is False
+
+
+def test_missing_character() -> None:
+    assert check_password("Password1") is False
+
+
+def test_too_long_password() -> None:
+    assert check_password("Pass@word123456789") is False
+
+
+def test_min_length_valid() -> None:
+    assert check_password("P@ssw1rd") is True
+
+
+def test_max_16_length_valid() -> None:
+    assert check_password("Valid@Passw0rd12") is True
+
+
+def test_invalid_character() -> None:
+    assert check_password("Password1*") is False
